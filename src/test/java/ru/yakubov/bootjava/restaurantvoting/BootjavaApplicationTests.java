1a0162792742b6085497f5aa5bfe99cf90a1f1be
source-store <73580046+source-store@users.noreply.github.com>
2021-04-30 10:38:18 +0300

    1_01_user_with_lombok.patch

---
+++ src/test/java/ru/yakubov/bootjava/restaurantvoting/BootjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakubov.bootjava.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
